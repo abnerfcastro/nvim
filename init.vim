35c7aedd24ec74338337180bedc87c6ff6a90675
Abner Castro <abnerfcastro@gmail.com>
2021-02-20 15:29:16 -0300

    Add plugin loading file

--- init.vim
+++ init.vim
@@ -1,8 +1,3 @@
 source $HOME/.config/nvim/general/settings.vim
 source $HOME/.config/nvim/keys/mappings.vim
-
-call plug#begin("$HOME/.vim/plugged")
-    " Plugin Section
-call plug#end()
-
-" Config Section
+source $HOME/.config/nvim/plugs/plugins.vim
--- install.sh
+++ install.sh
@@ -6,13 +6,17 @@ CONFIG_ROOT=$HOME/.config/nvim
 mkdir -p $CONFIG_ROOT
 mkdir -p $CONFIG_ROOT/general
 mkdir -p $CONFIG_ROOT/keys
+mkdir -p $CONFIG_ROOT/plugs
 
-# Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+if [ ! -f "$CONFIG_ROOT/autoload/plug.vim" ]; then
+    # Install vim-plug
+    sh -c 'curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
 
 # Copy init.vim file to config folder
 cp init.vim $CONFIG_ROOT/init.vim
 cp general/settings.vim $CONFIG_ROOT/general/settings.vim
 cp keys/mappings.vim $CONFIG_ROOT/keys/mappings.vim
+cp plugs/plugins.vim $CONFIG_ROOT/plugs/plugins.vim
 
